faef1686c8e0c96c961ce0fc6d929cd70d232626
Giuliani D Sanches <giulianisanches@users.noreply.github.com>
2022-05-09 15:39:33 -0300

    Test functions

---
+++ azurelog.sh
@@ -0,0 +1,19 @@
+function run {
+    $command = $1
+    
+    echo "##[command] $command"
+    
+    eval $command
+}
+
+function group {
+    echo "##[group] $1"
+}
+
+function endgroup {
+    echo "##[endgroup]"
+}
+
+function section {
+    echo "##[section] $1"
+}
